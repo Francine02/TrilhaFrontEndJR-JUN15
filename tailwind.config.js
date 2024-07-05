73f07fcaaa9fd8792b149e2cb2948411e84b111c
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-07-05 17:03:44 -0300

    Feat: Adicionado seção com as imagens das minhas habilidades. Nas configurações do Tailwind, adicionado um keyframe para as imagens da seção com as habilidades ficarem como um carrossel.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ module.exports = {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slide: {
+          '0%': { right: '-110%' },
+          '100%': {right: 'calc(+100% + 60rem)' }, // Adicionando margem extra para telas maiores
+        },
+      },
+      animation: {
+        slide: 'slide 17s linear infinite',
+      },
+    },
   },
   plugins: [],
-}
-
+};
